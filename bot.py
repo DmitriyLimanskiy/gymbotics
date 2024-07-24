e03bbe328f29c8902673098d7a49ccef29294402
Dmitriy Limanskiy <dmiitrii19s@gmail.com>
2024-07-24 22:08:30 +0300

    New life

--- bot.py
+++
@@ -1,7 +0,0 @@
-print("moya vetka don't vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  touch fucking bitch!")
-
-print("222")
-print("moya vetka don't touch fucking bitch!")
-print("2sd22")
-print("moya vetka don't touch fucking bitch!")
-print("2sd22")
