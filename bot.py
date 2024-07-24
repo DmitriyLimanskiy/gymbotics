681b080214d74221110e22cf700344103b8df5c3
Arthur <Art2003a27@mail.ru>
2024-07-24 22:04:53 +0300

    ASDSAD

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-print("moya vetka don't touch fucking bitch!")
+print("moya vetka don't vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  vetka don't  touch fucking bitch!")
+
 print("222")
 print("moya vetka don't touch fucking bitch!")
 print("2sd22")
